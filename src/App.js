cc3369c3359e987dc634eb4675dfd30d06cde2ba
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-09-20 20:40:11 -0300

    add - configura rotas da aplicaçao

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <div> Projeto Wallet </div>
-  );
-}
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={ Login } />
+    <Route path="/carteira" component={ Wallet } />
+    <Route path="*" component={ NotFound } />
+  </Switch>
+);
 
 export default App;
