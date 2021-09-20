0abac8b06ed3e22b4ac8b06a14c275f684d815f8
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-09-20 20:19:17 -0300

    add - configura reducer das despesas

---
+++ src/redux/reducer/wallet.js
@@ -0,0 +1,45 @@
+import { FETCH_ERROR_CASES,
+  FETCH_SUCCESS_COINS,
+  FETCH_SUCCESS_CURRENT,
+  REMOVE_EXPENSES,
+} from '../actions/index';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+  error: '',
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  const { type, error, response, payload, id } = action;
+  switch (type) {
+  case FETCH_SUCCESS_COINS:
+    return {
+      ...state,
+      currencies: response,
+    };
+  case FETCH_ERROR_CASES:
+    console.log(error);
+    return {
+      ...state,
+      error,
+    };
+  case FETCH_SUCCESS_CURRENT:
+    return {
+      ...state,
+      expenses: [
+        ...state.expenses,
+        payload,
+      ],
+    };
+  case REMOVE_EXPENSES:
+    return {
+      ...state,
+      expenses: state.expenses.filter((expense) => expense.id !== id),
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
