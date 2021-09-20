7d2ce448d5800232156925114424e18415e5b362
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-09-20 20:16:06 -0300

    add - configura modulo de exportaçao

---
+++ src/redux/actions/index.js
@@ -0,0 +1,19 @@
+export {
+  FETCH_ERROR_CASES,
+  FETCH_SUCCESS_CURRENT,
+  saveExpenses,
+} from './addExpense';
+
+export {
+  LOGIN,
+  addUser,
+} from './signIn';
+export {
+  REMOVE_EXPENSES,
+  removeExpenses,
+} from './removeExpense';
+
+export {
+  FETCH_SUCCESS_COINS,
+  fetchCoins,
+} from './getCoins';
