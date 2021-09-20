a325e507ead5ff882f2d8d5e8521c7232be09e6f
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-09-20 20:51:25 -0300

    add - configura componente button do formulario

---
+++ src/components/formsComponents/Button.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { string, func, bool } from 'prop-types';
+
+function Button(props) {
+  const { label, submit, disabled } = props;
+  return (
+    <button
+      type="button"
+      onClick={ submit }
+      disabled={ disabled }
+    >
+      { label }
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  disabled: false,
+};
+
+Button.propTypes = {
+  disabled: bool,
+  label: string.isRequired,
+  submit: func.isRequired,
+};
+
+export default Button;
