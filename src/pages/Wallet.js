ef404d5f81ff4111598b44fdda40df16961063be
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-09-20 20:41:21 -0300

    add - configura page wallet

---
+++ src/pages/Wallet.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/headerComponents/Header';
+import Form from '../components/formsComponents/Form';
+import Table from '../components/tableComponents/Table';
+
+function Wallet() {
+  return (
+    <section>
+      <Header />
+      <Form />
+      <Table />
+    </section>
+  );
+}
+
+export default Wallet;
