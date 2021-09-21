edb307089d222101e871c40a4ec5032e706faaea
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-09-20 22:37:30 -0300

    add - configura arquivo que contem as options do componente select

---
+++ src/helper/optionsSelect.js
@@ -0,0 +1,35 @@
+export const methods = [
+  {
+    val: 'Dinheiro',
+    lab: 'Dinheiro',
+  },
+  {
+    val: 'Cartão de crédito',
+    lab: 'Cartão de crédito',
+  },
+  {
+    val: 'Cartão de débito',
+    lab: 'Cartão de débito',
+  }];
+
+export const category = [
+  {
+    val: 'Alimentação',
+    lab: 'Alimentação',
+  },
+  {
+    val: 'Lazer',
+    lab: 'Lazer',
+  },
+  {
+    val: 'Trabalho',
+    lab: 'Trabalho',
+  },
+  {
+    val: 'Transporte',
+    lab: 'Transporte',
+  },
+  {
+    val: 'Saúde',
+    lab: 'Saúde',
+  }];
