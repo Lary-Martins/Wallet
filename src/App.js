e053743bc9883b82d31ca77e1ed30f340bb6b4ab
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-09-23 00:10:22 -0300

    fix - sitema de rotas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 import NotFound from './pages/NotFound';
 
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={ Login } />
-    <Route path="/carteira" component={ Wallet } />
-    <Route path="*" component={ NotFound } />
-  </Switch>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/carteira" component={ Wallet } />
+      <Route path="*" component={ NotFound } />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
