fad0fd051cef5d73e871ea20e51648d1dc5b60bf
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-09-23 00:14:30 -0300

    fix - problema de importaçao da dependencia

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import registration from './reducers/registration';
-import wallet from './reducers/wallet';
+import registration from './reducer/registration';
+import wallet from './reducer/wallet';
 
 const store = createStore(
   combineReducers({
