8d6c90de25b7b1792303567a5dc1c0914a041175
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-10-11 22:30:43 -0300

    add - caso de expenseDetails

--- src/redux/reducer/wallet.js
+++ src/redux/reducer/wallet.js
@@ -1,17 +1,22 @@
 import { FETCH_ERROR_CASES,
   FETCH_SUCCESS_COINS,
   FETCH_SUCCESS_CURRENT,
-  REMOVE_EXPENSES,
+  EXPENSE_DETAILS,
+  REMOVE_EXPENSE,
+  EDIT_EXPENSE,
 } from '../actions/index';
 
 const INITIAL_STATE = {
   currencies: [],
+  expenseDetail: {},
+  editExpense: {},
+  edit: false,
   expenses: [],
   error: '',
 };
 
 const wallet = (state = INITIAL_STATE, action) => {
-  const { type, error, response, payload, id } = action;
+  const { type, error, response, payload, id, edit } = action;
   switch (type) {
   case FETCH_SUCCESS_COINS:
     return {
@@ -27,15 +32,27 @@ const wallet = (state = INITIAL_STATE, action) => {
   case FETCH_SUCCESS_CURRENT:
     return {
       ...state,
+      edit,
       expenses: [
         ...state.expenses,
         payload,
       ],
     };
-  case REMOVE_EXPENSES:
+  case REMOVE_EXPENSE:
     return {
       ...state,
       expenses: state.expenses.filter((expense) => expense.id !== id),
+    };
+  case EDIT_EXPENSE:
+    return {
+      ...state,
+      editExpense: state.expenses.find((expense) => expense.id === id),
+      edit,
+    };
+  case EXPENSE_DETAILS:
+    return {
+      ...state,
+      expenseDetail: state.expenses.find((expense) => expense.id === id),
     };
   default:
     return state;
