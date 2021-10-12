392062b2813620ce57dca655deb330063f47daec
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-10-11 22:43:20 -0300

    create - atributos de cada tag

---
+++ src/helper/imgTags.js
@@ -0,0 +1,8 @@
+const imgTags = {
+  Alimentação: { img: 'https://i.pinimg.com/564x/16/a1/57/16a157bc59049f73710b060601d03ee9.jpg', color: '#F9A36A' },
+  Lazer: { img: 'https://i.pinimg.com/564x/cc/22/77/cc2277b555cd6afc7cba39aaa7cb3864.jpg', color: '#FFC9D7' },
+  Trabalho: { img: 'https://i.pinimg.com/564x/7d/4d/ac/7d4dac5e7dfe5c34cd2e097e7ce37adc.jpg', color: '#1BB1B2' },
+  Transporte: { img: 'https://i.pinimg.com/564x/49/f9/3a/49f93a58c612e304a9fff370d2f68d59.jpg', color: '#EF3E38' },
+  Saúde: { img: 'https://i.pinimg.com/564x/95/da/b9/95dab9ad6a27ab7f2f5ce3fbad2f795d.jpg', color: '#56F7E7' },
+};
+export default imgTags;
