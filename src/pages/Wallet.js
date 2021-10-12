b0890590d796edb1170f582b731707846b8ffecd
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-10-11 22:24:59 -0300

    remove - remove componentes da page Wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Header from '../components/headerComponents/Header';
-import Form from '../components/formsComponents/Form';
-import Table from '../components/tableComponents/Table';
+import AddForm from '../components/formsComponents/AddForm';
+import EditForm from '../components/formsComponents/EditForm';
+import Main from '../components/mainComponents/Main';
 
 function Wallet() {
+  const { edit } = useSelector((state) => state.wallet);
+
   return (
     <section>
       <Header />
-      <Form />
-      <Table />
+      { edit ? <EditForm /> : <AddForm />}
+      <Main />
     </section>
   );
 }
